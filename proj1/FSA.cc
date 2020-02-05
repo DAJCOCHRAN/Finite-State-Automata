44cdd347e06127c478acd59f8a4137efc4d8e7c4
Donald <dajcochran@gmail.com>
2020-02-05 15:39:38 -0500

    Reads file, collects variables

--- proj1/FSA.cc
+++ proj1/FSA.cc
@@ -12,6 +12,7 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -19,7 +20,30 @@ using namespace std;
 FSA::FSA(ifstream &ifs)
 {
     cout << "called stub version of FSA constructor\n";
+    
+    if(!ifs.is_open()){ //If the file is not open we have a problem
+        exit(EXIT_FAILURE);    
+    }
+
+    char word[2];
+    ifs >> word; //first value from document which should be the alphabet
+    sigma = word;
+    int index=0;//to track each element we itterate through in the while 
+    while(ifs.good()){ //while we are not to the end of the file
+        cout<< word << " "; //print word from file pointer
+        if(index==1){ //This should be state count
+            num_states = int(word);
+        }
+        if(index == 2){//This should be the start state 
+            start_state = int(word);
+        }
+        ifs >> word;//get to next word
+        index++;
+    }
+    cout<<word<<endl; //last character to be printed
+
     // do stuff here, the last action being
+
     get_state_table(ifs);
 }
 
